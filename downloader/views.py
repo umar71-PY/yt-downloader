d83500db19e6a023eeb90022a9eb1ad9bce0c8f1
umar71-PY <umarasifmirza439@gmail.com>
2026-06-30 22:29:45 +0500

    Update views.py

--- downloader/views.py
+++ downloader/views.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from .models import VideoDownload
 
 def home(request):
-    # Yeh user ko main search page par le kar jayega
     return render(request, 'downloader/home.html')
 
 def fetch_video_info(request):
@@ -18,14 +17,12 @@ def fetch_video_info(request):
         ydl_opts = {}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             try:
-                # Video ki details nikaalo bina download kiye
                 info = ydl.extract_info(video_url, download=False)
                 video_title = info.get('title', 'Awesome Video')
                 formats = info.get('formats', [])
                 
                 available_formats = []
                 
-                # 🎵 JADOO 1: MP3 Audio ka VIP option sab se oopar daal diya!
                 available_formats.append({
                     'format_id': 'bestaudio',
                     'resolution': '🎵 MP3 Audio Only',
@@ -33,7 +30,6 @@ def fetch_video_info(request):
                     'filesize': 'N/A'
                 })
 
-                # 🎬 JADOO 2: Sirf MP4 Videos ko filter kiya
                 for f in formats:
                     if f.get('vcodec') != 'none' and f.get('ext') == 'mp4':
                         size_mb = round(f.get('filesize', 0) / (1024 * 1024), 2) if f.get('filesize') else 'N/A'
@@ -49,7 +45,6 @@ def fetch_video_info(request):
                     'title': video_title,
                     'formats': available_formats
                 }
-                # HTML front-end par bhej do jahan user quality select karega
                 return render(request, 'downloader/select_quality.html', context)
                 
             except Exception as e:
@@ -63,7 +58,6 @@ def download_video(request):
         format_id = request.POST.get('format_id')
         video_title = request.POST.get('title', 'video')
         
-        # Laptop ki storage mein temporary folder banao
         download_dir = os.path.join(settings.BASE_DIR, 'downloads')
         if not os.path.exists(download_dir):
             os.makedirs(download_dir)
@@ -72,7 +66,6 @@ def download_video(request):
             'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
         }
 
-        # 🎵 THEKEDARI HACK: Agar user ne MP3 select kiya hai
         if format_id == 'bestaudio':
             ydl_opts['format'] = 'bestaudio/best'
             ydl_opts['postprocessors'] = [{
@@ -80,7 +73,6 @@ def download_video(request):
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
             }]
-        # 🎬 THEKEDARI HACK: Agar Video select ki hai (Video + Audio FFMPEG merge karega)
         else:
             ydl_opts['format'] = f"{format_id}+bestaudio/best"
             ydl_opts['merge_output_format'] = 'mp4'
@@ -89,7 +81,6 @@ def download_video(request):
             try:
                 info = ydl.extract_info(video_url, download=True)
                 
-                # File ka exact naam uthao download hone ke baad
                 if 'requested_downloads' in info:
                     filename = info['requested_downloads'][0]['filepath']
                 else:
@@ -99,14 +90,12 @@ def download_video(request):
                     else:
                         filename = os.path.splitext(filename)[0] + '.mp4'
                 
-                # History database mein save karo
                 VideoDownload.objects.create(
                     url=video_url,
                     title=video_title,
                     quality=format_id
                 )
                 
-                # File open karke direct user ko download ke liye serve karo
                 response = FileResponse(open(filename, 'rb'), as_attachment=True)
                 return response
                 
